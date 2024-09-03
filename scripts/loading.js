6d3216e90cd7203681363b62d3cbdeedc412e08b
wangfengjun <hiwjcn@live.com>
2024-09-03 19:30:26 +0800

    998877

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,67 +1,23 @@
-/**
- * loading 占位
- * 解决首次加载时白屏的问题
- */
-const innerHTML = `
-<style>
-        html,
-        body,
-        #root {
-          margin: 0;
-          padding: 0;
-        }
-        #root {
-          background-repeat: no-repeat;
-          background-size: 100% auto;
-        }
-      </style>
-
+function set_loading() {
+  /**
+   * loading 占位
+   * 解决首次加载时白屏的问题
+   */
+  const innerHTML = `
       <style>
-      .loading {
-        display: block;
-        position: relative;
-        width: 6px;
-        height: 10px;
-        animation: rectangle infinite 1s ease-in-out -0.2s;
-        background-color: #000;
+      .loader {
+        font-weight: bold;
+        font-family: sans-serif;
+        font-size: 30px;
+        animation: l1 1s linear infinite alternate;
       }
-
-      .loading:before,
-      .loading:after {
-        position: absolute;
-        width: 6px;
-        height: 10px;
-        content: "";
-        background-color: #000;
+      .loader:before {
+        content:"Loading..."
       }
-
-      .loading:before {
-        left: -14px;
-        animation: rectangle infinite 1s ease-in-out -0.4s;
-      }
-
-      .loading:after {
-        right: -14px;
-        animation: rectangle infinite 1s ease-in-out;
-      }
-
-      @keyframes rectangle {
-        0%,
-        80%,
-        100% {
-          height: 20px;
-          box-shadow: 0 0 #000;
-        }
-
-        40% {
-          height: 30px;
-          box-shadow: 0 -20px #000;
-        }
-      }
+      @keyframes l1 {to{opacity: 0}}
       </style>
 
       <div style='position: absolute;top:0;right:0;bottom:0;left:0;z-index: 10;'>
-
         <div style='
           display: flex;
           flex-direction: column;
@@ -70,19 +26,18 @@ const innerHTML = `
           height: 100%;
           min-height: 362px;
         '>
-          <div class='loading' style='margin-bottom:10px'></div>
-          <h3>正在加载中...</h3>
+          <div class='loader'></div>
         </div>
-
       </div>
 `;
 
-(function() {
   const app_container = document.querySelector('#root');
   if (app_container && app_container.innerHTML?.trim().length <= 0) {
     app_container.innerHTML = innerHTML;
   }
+}
 
+function set_header_meta() {
   const header = document.getElementsByTagName('head')?.item(0);
   if (header) {
     const meta = document.createElement('meta');
@@ -90,5 +45,9 @@ const innerHTML = `
     meta.setAttribute('content', 'upgrade-insecure-requests');
     header.appendChild(meta);
   }
+}
 
+(function() {
+  set_loading();
+  //set_header_meta();
 })();
